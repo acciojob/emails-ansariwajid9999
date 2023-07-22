06ce47c90b79f901ffe847bb35442a91a16f1bc5
Wajid <ansariwajid9999@gmail.com>
2023-07-22 22:51:41 +0530

    code changed

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -2,64 +2,134 @@ package com.driver;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class Gmail extends Email {
 
     int inboxCapacity; //maximum number of mails inbox can store
+    Deque<Mail> inbox;
+    //addLast ---> mail Addition..
+    //removeFirst()...
+    ArrayList<Mail>trash;
     //Inbox: Stores mails. Each mail has date (Date), sender (String), message (String). It is guaranteed that message is distinct for all mails.
     //Trash: Stores mails. Each mail has date (Date), sender (String), message (String)
     public Gmail(String emailId, int inboxCapacity) {
-
+        super(emailId);
+        this.inboxCapacity=inboxCapacity;
+        inbox=new LinkedList<>();
+        trash=new ArrayList<>();
     }
 
     public void receiveMail(Date date, String sender, String message){
+        if(inbox.size()==inboxCapacity){
+            Mail mail=inbox.removeFirst();
+            trash.add(mail);
+        }
+        inbox.add(new Mail(date,sender,message));
         // If the inbox is full, move the oldest mail in the inbox to trash and add the new mail to inbox.
         // It is guaranteed that:
         // 1. Each mail in the inbox is distinct.
         // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
-
     }
 
     public void deleteMail(String message){
+        for(Mail mail:inbox){
+            if(mail.getMessage().equals(message)==true){
+                trash.add(mail);
+                inbox.remove(mail);
+                break;
+            }
+        }
         // Each message is distinct
         // If the given message is found in any mail in the inbox, move the mail to trash, else do nothing
 
     }
 
     public String findLatestMessage(){
+        if(inbox.size()==0)return null;
+        Mail mail=inbox.getLast();
+        return mail.getMessage();
         // If the inbox is empty, return null
         // Else, return the message of the latest mail present in the inbox
 
     }
 
     public String findOldestMessage(){
+        if(inbox.size()==0)return null;
+        Mail mail=inbox.getFirst();
+        return mail.getMessage();
         // If the inbox is empty, return null
         // Else, return the message of the oldest mail present in the inbox
 
     }
 
     public int findMailsBetweenDates(Date start, Date end){
+        int cnt=0;
+        for(Mail mail:inbox){
+            Date date=mail.getDate();
+            int compareStart=date.compareTo(start);
+            int compareEnd=date.compareTo(end);
+            if(compareEnd==0 || compareStart==0 || (compareStart>0 && compareEnd<0)) cnt++;
+        }
+        return cnt;
         //find number of mails in the inbox which are received between given dates
         //It is guaranteed that start date <= end date
 
     }
 
     public int getInboxSize(){
+        return inbox.size();
         // Return number of mails in inbox
 
     }
 
     public int getTrashSize(){
+        return trash.size();
         // Return number of mails in Trash
 
     }
 
     public void emptyTrash(){
+        trash=new ArrayList<>();
         // clear all mails in the trash
 
     }
 
     public int getInboxCapacity() {
+        return inboxCapacity;
         // Return the maximum number of mails that can be stored in the inbox
+    }
+    class Mail{
+        private Date date;
+        private String sender;
+        private String message;
+        public Mail(Date date,String sender,String message){
+            this.date=date;
+            this.sender=sender;
+            this.message=message;
+        }
+        public Date getDate(){
+            return date;
+        }
+        public void setDate(Date date){
+            this.date = date;
+        }
+        public String getSender(){
+            return sender;
+        }
+        public void setSender(String sender){
+            this.sender = sender;
+        }
+        public String getMessage(){
+            return message;
+        }
+        public void setMessage(String message){
+            this.message = message;
+        }
+        @Override
+        public String toString(){
+            return ""+date;
+        }
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -11,16 +11,46 @@ public class Workspace extends Gmail{
     private ArrayList<Meeting> calendar; // Stores all the meetings
 
     public Workspace(String emailId) {
+        super(emailId,Integer.MAX_VALUE);
+        calendar=new ArrayList<>();
         // The inboxCapacity is equal to the maximum value an integer can store.
 
     }
 
     public void addMeeting(Meeting meeting){
+        calendar.add(meeting);
         //add the meeting to calendar
 
     }
 
     public int findMaxMeetings(){
+        if(calendar.size()==0) return 0;
+        Collections.sort(calendar,(a,b)-> {
+            if(a.getStartTime().isAfter(b.getStartTime())) return 1;
+            else if(a.getStartTime().isBefore(b.getStartTime())) return -1;
+            else return 0;
+        });
+        int cnt=0;
+        LocalTime pStart=calendar.get(0).getStartTime();
+        LocalTime pEnd=calendar.get(0).getEndTime();
+        int n=calendar.size();
+        for(int i=1;i<n;i++){
+            LocalTime cStart=calendar.get(i).getStartTime();
+            LocalTime cEnd=calendar.get(i).getEndTime();
+            if(cStart.isAfter(pEnd)){
+                cnt++;
+                pStart=cStart;
+                pEnd=cEnd;
+            }else{
+                //means overlapping..
+                if(pEnd.isAfter(cEnd)){
+                    pStart=cStart;
+                    pEnd=cEnd;
+                }
+            }
+        }
+        return cnt+1;
+
         // find the maximum number of meetings you can attend
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
